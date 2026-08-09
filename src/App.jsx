86561ff359b88f72eeb5c01f49e8fc12ffbcf222
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-08-09 13:08:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="p-8 bg-white rounded-lg shadow-md text-center">
+        <h1 className="text-3xl font-bold text-blue-600 mb-4">
+          BDE Events - React & Laravel
+        </h1>
+        <p className="text-gray-600">Tailwind CSS خدام مزيان!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // هاد السطر كيعني: قلب فكاع الملفات لي وسط src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
