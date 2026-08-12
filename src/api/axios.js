011524e7294653dd941072289cbfba754fb225a7
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-08-12 21:55:40 +0100

    Update axios base URL to version 3 for API integration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://bde-events-api-v1.onrender.com/api',
+    baseURL: 'https://bde-events-api-v3.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
